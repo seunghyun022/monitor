c353fb9a1dbf3b0a3c21057602fd1754cde73d92
seunghyun022 <onyshup@naver.com>
2022-04-15 04:25:44 -0700

    psutil with tqdm

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from tqdm import tqdm
+from time import sleep
+import psutil
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with tqdm(total=100, desc='cpu%', position=1) as cpubar, tqdm(total=100, desc='ram%', position=0) as rambar:
+    while True:
+        rambar.n=psutil.virtual_memory().percent
+        cpubar.n=psutil.cpu_percent()
+        rambar.refresh()
+        cpubar.refresh()
+        sleep(0.5)
