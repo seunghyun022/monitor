eeb4e5fc467118b4bcda60e0b784a22c34de902e
onyshup <onyshup@naver.com>
2022-04-16 13:01:56 -0700

    DB connection and manipulation

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from sqlite3 import IntegrityError
 import psutil
-print(psutil.virtual_memory().percent)
-print(psutil.cpu_percent())
+import pymysql
+import mysql_auth
+import socket
+import datetime
+import pytz
+login = mysql_auth.Info
+
+ip=socket.gethostbyname(socket.gethostname())
+cpu=int(psutil.virtual_memory().percent)
+mem=int(psutil.cpu_percent())
+now=datetime.datetime.now(pytz.timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S')
+
+conn = pymysql.connect(host=login['host'],user=login['user'],passwd=login['passwd'],db=login['db'],charset=login['charset'])
+c= conn.cursor()
+try:
+    c.execute('INSERT INTO mon(Ip,Cpu,Mem,Updated) VALUES(%s,%s,%s,%s) ON DUPLICATE KEY UPDATE Cpu=%s, Mem=%s, Updated=%s',
+    (ip,cpu,mem,now,cpu,mem,now))
+    conn.commit()
+except:
+    print('insert failed')
+conn.close
+
